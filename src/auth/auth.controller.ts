f3d351b4fe4c7a9be369239055a0af00cd569f2d
Khoaha <80685180+Khoagoodkid@users.noreply.github.com>
2025-10-01 12:55:00 -0500

    feat: login and create token functions

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,17 +3,28 @@ import { AuthService } from "./auth.service";
 import { Body } from "@nestjs/common";
 import { CreateUserDto } from "src/user/dto/create-user.dto";
 import { CreatedResponse } from "src/core/successResponse";
+import { LoginDto } from "./dto/login.dto";
+import { FastifyReply } from "fastify";
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {
     }
 
+    @Post('login')
+    async login(@Body() loginDto: LoginDto, reply: FastifyReply) {
+        return reply.send(new CreatedResponse({
+            data: await this.authService.login(loginDto, reply),
+            message: 'User logged in successfully',
+        }));
+    }
+
+
     @Post('register')
-    async register(@Body() user: CreateUserDto) {
-        return new CreatedResponse({
+    async register(@Body() user: CreateUserDto, reply: FastifyReply) {
+        return reply.send(new CreatedResponse({
             data: await this.authService.register(user),
             message: 'User registered successfully',
-        })
+        }));
     }
 }
